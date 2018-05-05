5311ea825d445a42003169a60dad150bdddc52f3
Andrew Neidley <neidz44@gmail.com>
2018-05-05 07:57:12 -0500

    initial static site template app

--- site/public/js/app.js
+++ site/public/js/app.js
@@ -21,7 +21,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     // 2. Display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.src = 'images/dice-' + dice + '.png';
 
 
     // 3. Update the round score IF the rolled number was NOT a 1
@@ -45,7 +45,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     // Check if player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
